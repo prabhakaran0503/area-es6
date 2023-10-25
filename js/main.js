340c64dc33436891067095337a1f98c975b33e58
prabhakaran <prabhakaranplkk@gmail.com>
2023-10-25 10:06:25 +0530

    area

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let length=prompt("enter the length");
+let width=prompt("enter the width");
+let fun=function(a,b){
+    let ans=a*b
+    return ans
+}
+let res=fun(length,width);
+document.write(res)
